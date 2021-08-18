5ae8755a163fd4d57f20a0672c489210d7bc9272
Rocco P <roccopan@gmail.com>
2021-08-18 14:38:48 -0400

    0.2

--- youtube-like-ratio.js
+++ youtube-like-ratio.js
@@ -1,25 +1,42 @@
 window.addEventListener(`load`, () => {
+	// Obtain exact number of likes/dislikes from the first sentiment tooltip
 	const str = document.querySelector('#sentiment #tooltip').textContent.replaceAll('\n','').replaceAll(',', '')
 	const up = parseInt((str).split(' / ')[0])
 	const down = parseInt((str).split(' / ')[1])
 	
+	// Setup the styling for the new component
 	const style = document.createElement('style')
 	document.head.appendChild(style)
 	style.sheet.insertRule(`div#top-level-buttons-computed::before {
 			content: "${Math.round(up / (up+down) * 100)}%";
+			display: grid;
+			box-sizing: border-box;
+			height: 2.2em;
+			padding: 0em 0.4em 0.125em;
+			border: 2px solid var(--yt-spec-grey-3);
+			border-radius: 0.25em;
+			margin-right: 0.25em;
+			align-content: center;
+			align-self: center;
 			font-size: var(--ytd-tab-system_-_font-size, 1.4em);
 			font-weight: var(--ytd-tab-system_-_font-weight, 700);
 			letter-spacing: var(--ytd-tab-system_-_letter-spacing, 0.05em);
-			display: grid;
-			place-content: center;
-			color: var(--yt-button-icon-button-text-color, var(--yt-spec-text-secondary, inherit));
-			padding-bottom: 0.3em;
-			padding-right: 0.4em;
-			padding: 0 0.4em 0.3em;
-			box-sizing: border-box;
+			color: var(--yt-spec-grey-4);
 		}`
 	)
-	document.querySelector('#sentiment').style.width = `100%`
-	document.querySelector('#sentiment #container').style.backgroundColor = `var(--yt-brand-youtube-red, #f00)`
-	document.querySelector('#sentiment #like-bar').style.backgroundColor = `var(--yt-spec-light-green, #2ba640)`
+	style.sheet.insertRule(`#sentiment {
+			width: 100% !important;
+			padding-top: 2px !important;
+		}`
+	)
+	style.sheet.insertRule(`#sentiment #container {
+			background-color: var(--yt-brand-youtube-red, #f00) !important;
+			height: 6px !important;
+		}`
+	)
+	style.sheet.insertRule(`#sentiment #like-bar {
+			background-color: var(--yt-spec-light-green, #2ba640) !important;
+			height: 100% !important;
+		}`
+	)
 })
